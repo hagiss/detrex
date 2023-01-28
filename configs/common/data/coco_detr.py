2087629b47854a7e990b21c7b496572a4ab71646
hagiss <66506438+hagiss@users.noreply.github.com>
2023-01-28 15:22:52 +0900

    asd

--- configs/common/data/coco_detr.py
+++ configs/common/data/coco_detr.py
@@ -56,7 +56,7 @@ def MyMapper(dataset_dict):
     return dataset_dict
 
 
-def MyMapper(dataset_dict):
+def TestMapper(dataset_dict):
 
     dataset_dict = copy.deepcopy(dataset_dict)
     image = utils.read_image(dataset_dict['file_name'], format='BGR')
