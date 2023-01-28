fe4fae6ee5fa3676bc9dd5208679194a174a326e
hagiss <66506438+hagiss@users.noreply.github.com>
2023-01-28 19:20:01 +0900

    asd

--- tools/train_net.py
+++ tools/train_net.py
@@ -159,11 +159,11 @@ def do_test(cfg, model):
     test_loader = instantiate(cfg.dataloader.test)
 
     for data in tqdm(test_loader):
-        data = data[0]
+        # data = data[0]
         prediction_string = ''
         print("data", data)
-        outputs = model(data)['instances']
-        # print("outputs", outputs)
+        outputs = model(data)
+        print("outputs", outputs)
         if torch.cuda.is_available():
             torch.cuda.synchronize()
         targets = outputs.pred_classes.cpu().tolist()
