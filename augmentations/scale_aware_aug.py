c3da1d757a4139721726bdc8f1320224be7679ff
hagiss <66506438+hagiss@users.noreply.github.com>
2023-01-30 23:02:31 +0900

    asd

--- augmentations/scale_aware_aug.py
+++ augmentations/scale_aware_aug.py
@@ -58,7 +58,7 @@ class SA_Aug(object):
 
         dataset_dict['instances']._image_size = tensor_out.shape[1:]
         dataset_dict['instances']._fields = target_out
-        print(tensor_out.shape)
+        # print(tensor_out.shape)
         dataset_dict['image'] = tensor_out #.transpose(2, 0, 1)
 
         return dataset_dict
