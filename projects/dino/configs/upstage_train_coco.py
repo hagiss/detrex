9abbd160b36aa10ebcc5288d8896710ef0950e18
hagiss <66506438+hagiss@users.noreply.github.com>
2023-01-30 11:58:30 +0900

    asd

--- projects/dino/configs/upstage_train_coco.py
+++ projects/dino/configs/upstage_train_coco.py
@@ -12,9 +12,9 @@ train = get_config("common/train.py").train
 
 
 # modify training config
-train.init_checkpoint = "../checkpoints/dino_vitdet_large_4scale_50ep.pth"
+# train.init_checkpoint = "../checkpoints/dino_vitdet_large_4scale_50ep.pth"
 # train.init_checkpoint = "../checkpoints/model_final.pth"
-# train.init_checkpoint1 = "./output/coco2/model_0014999.pth"
+train.init_checkpoint = "./output/coco_large/model_0019999.pth"
 # train.init_checkpoint2 = "./output/coco2/model_0013499.pth"
 # train.init_checkpoint3 = "./output/coco2/model_0011999.pth"
 train.output_dir = "./output/coco_large"
